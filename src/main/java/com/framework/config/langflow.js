a8fca954b7baecea18a7eae516efd05a886cc810
Sharathmathi <67513732+Sharathmathi@users.noreply.github.com>
2025-11-17 21:51:46 +0530

    Update langflow.js

--- src/main/java/com/framework/config/langflow.js
+++ src/main/java/com/framework/config/langflow.js
@@ -1,1 +1,29 @@
+// -------------------------
+// Java Code Snippet
+// -------------------------
+/*
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+}
+*/
 
+// -------------------------
+// Python Code Snippet
+// -------------------------
+/*
+def greet():
+    print("Hello from Python!")
+
+greet()
+*/
+
+// -------------------------
+// JavaScript Code Snippet
+// -------------------------
+function helloJS() {
+    console.log("Hello from JavaScript!");
+}
+
+helloJS();
